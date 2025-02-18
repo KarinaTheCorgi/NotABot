317f2a934833195177eeee643940e828106f5a7e
Karina <karinafsolis55@gmail.com>
2025-02-18 11:12:25 -0700

    updated bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,12 @@
+""""
+
+Resources:
+    - Setting Up Bot 
+        - https://realpython.com/how-to-make-a-discord-bot-python/
+    - Commands
+        - https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html
+"""
+
 import os
 import discord
 from discord.ext import commands
@@ -7,16 +16,21 @@ load_dotenv()
 token = os.getenv('token')
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-
+polling_int = 6000
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-
+#Commands, might want to seperate into another file when this list gets bigger
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+    
+@bot.command()
+async def changeTime(ctx, time:float):
+    polling_int = time
+    await ctx.send(f'Changed Polling time to: {time} secs')
 
 
 bot.run(token)
