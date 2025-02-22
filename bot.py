4413133decafcf26c2c50ad77c788b5c06921c38
Karina <karinafsolis55@gmail.com>
2025-02-21 22:05:33 -0700

    update start command

--- bot.py
+++ bot.py
@@ -118,7 +118,8 @@ async def start(ctx, prompt_time:int = 10800, reply_time:int = 5):
             reply_time = reply_time + 10
     
     check_sql = "SELECT COUNT(*) FROM Users WHERE author_id = %s"
-    db.cursor().execute(check_sql, (str(ctx.author.id)))
+    val = (str(ctx.author.id))
+    db.cursor().execute(check_sql, val)
     
     if db.cursor().fetchone()[0] > 0:
         sleep(3)
