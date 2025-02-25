2296efe8c76e5fdddd3b7774753b087ba891b8b0
Karina <karinafsolis55@gmail.com>
2025-02-25 10:25:27 -0700

    separating commands into different files

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 """"
 File: bot.py
 Authors: 
-    - Asher Adighije
-    - Taylor Hobbs
-    - Kyle Miller
     - Karina Solis
 
 Resources:
@@ -35,6 +32,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 import mysql.connector
+import settings
 
 def git_pull():
     if "Already up to date." in str(subprocess.run(["git", "pull"], capture_output=True, text=True)):
@@ -54,105 +52,23 @@ def update():
 # Creates a new (background) thread to auto update from git          
 Thread(target=update, daemon=True).start()
 
-
 # Loading Token and Creating Bot Variables
 load_dotenv()
 token = os.getenv('token')
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 
-# Connect to a database
-db_host = os.getenv('db_host')
-db_port = os.getenv('db_port')
-db_user = os.getenv('db_user')
-db_pass = os.getenv('db_pass')
-db_database_name = os.getenv('settings_db')
-
-db = mysql.connector.connect(
-  host = db_host,
-  port = db_port,
-  user = db_user,
-  password = db_pass,
-  database = db_database_name
-)
-
-
-
 # Event Listeners, might want to seperate into another file when this list gets bigger
 @bot.event
 async def on_ready():
+    await bot.get_cog('Settings')
     print(f'We have logged in as {bot.user}')    
 
-# Commands, also might want to seperate into another file when this list gets bigger
+# Test command
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong....')
-    print(ctx.message.author.id)
     
-    
-@bot.command()
-async def start(ctx, prompt_time:int = 10800, reply_time:int = 5):
-    # Starts polling loop, creates custom settings data
-    """
-    creates Settings for specified user
-    """
-    #it dont work imma cry
-    """
-    # prompt_time: time between each initial prompt given
-    if prompt_time < 30:
-        if prompt_time <= 0:
-            sleep(3)
-            await ctx.reply(f"Nice try. I'll give you 30 seconds between each prompt. I need time to think.")
-            prompt_time = 30
-        else:
-            sleep(3)
-            await ctx.reply(f"{prompt_time} seconds is stupid. Best I can get to you is {prompt_time + 30} seconds.")
-            prompt_time = prompt_time + 30
-    
-    # reply_time: how long the bot will give the user to respond
-    if reply_time < 5:
-        if reply_time <= 0:
-            sleep(3)
-            await ctx.reply(f"Ok smart guy. I'll give you 10 seconds for an answer. After that, I won't expect anything from you.")
-            reply_time = 10
-        else:
-            sleep(3)
-            await ctx.reply(f"{reply_time} seconds is unreasonable. I can wait around for {reply_time + 10} seconds.")
-            reply_time = reply_time + 10
-    
-    val = (ctx.author.id,)
-    db.cursor().execute("SELECT COUNT(*) FROM Users WHERE author_id = %s", val)
-    result = db.cursor().fetch()
-    
-    if result and int(result[0]) > 0:
-        sleep(3)
-        await ctx.reply("You're already on my radar. Please stop before you start again.")   
-    else:
-        insert_sql = "INSERT INTO Users (author_id, prompt_time, reply_time) VALUES (%s, %s, %s)"
-        val = (str(ctx.author.id), str(prompt_time), str(reply_time))
-        db.cursor().execute(insert_sql, val)
-        db.commit()
-        
-        sleep(3)
-        await ctx.send(f"Oop you've just added your name to a list of people for me to bug. I'll try to annoy you every {prompt_time} seconds")
-    """
-    ...
-    
-
-    
-@bot.command()
-async def stop(ctx):
-    # exits polling loop
-    """
-    destroys the 
-    """
-    ...
-
-    
-@bot.command()
-async def changeTime(ctx, time:float):
-    polling_int = time
-    await ctx.send(f'Changed Polling time to: {time} secs')
-    
-
+# Adds SettingsCmds to the bot
+bot.add_cog(settings.SettingsCmds(bot))
 bot.run(token)
---
+++ settings.py
@@ -0,0 +1,101 @@
+"""
+File: settings.py
+Authors: 
+    - Karina Solis
+Resources:
+    - Cogs
+        - https://discordpy.readthedocs.io/en/stable/ext/commands/cogs.html
+"""
+#i literally dk what im doing or if any of this makes sense. theres probably a much easier and simpler way to be doing this but im tired and scared
+import os
+import sys
+import subprocess
+from time import sleep
+from threading import Thread
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+import mysql.connector
+
+
+
+class Settings(commands.Cog):
+    def __init__(self, user):
+        # Connect to a database
+        load_dotenv() #unsure if needed
+
+        self.db = mysql.connector.connect(
+            host = os.getenv('db_host'),
+            port = os.getenv('db_port'),
+            user = os.getenv('db_user'),
+            password = os.getenv('db_pass'),
+            database = os.getenv('settings_db')
+        )
+        self.user = user
+        
+    async def isInDB(self)->bool:
+        """
+        checks if the user is in the database already
+        """ 
+        val = (self.user,)
+        self.db.cursor().execute("SELECT COUNT(*) FROM Users WHERE author_id = %s", val)
+        result = self.db.cursor().fetch()
+        
+        if result and int(result[0]) > 0:
+            return True  
+        return False
+        
+    def areArgsNorm(self, *args)->bool:
+        """
+        checks if the args are valid 
+        """
+        ...
+        
+    async def validateArgs(self, *args):
+        if Settings.areArgsNorm(*args):  
+            return
+        else:
+            # make em norm idk
+            ...
+        
+        
+class SettingsCmds():
+    """
+    the command cog for the bot to implement the setting changes
+    """
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send('Hello. Cmds were loaded.')
+    
+    @commands.command()
+    async def start(self, ctx, *args):
+        # Starts polling loop, creates custom settings data
+        """
+        creates Settings for specified user if there isnt one
+        """
+        settings = self.bot.get_cog('Settings')
+        if settings is not None:
+            if settings.isInDB(ctx.author.id):
+                await ctx.send("You're already in the database.")
+            else:
+                await settings.validateArgs(*args)
+                await settings.addUser(ctx.author.id, *args)
+        
+    @commands.command()
+    async def stop(self, ctx):
+        # exits polling loop
+        """
+        destroys the entry in db from specified user if there is one
+        """
+        settings = self.bot.get_cog('Settings')
+        if settings is not None:
+            if settings.isInDB(ctx.author.id):
+                await settings.remove_user(ctx.author.id)
+            else:
+                await ctx.send("You're not in the database.")
+            
