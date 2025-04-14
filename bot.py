d44156bb92be0f6a29a29a53d37c304db1b59bf6
Karina <karinafsolis55@gmail.com>
2025-04-14 13:11:35 -0700

    awaiting cog loads

--- bot.py
+++ bot.py
@@ -54,8 +54,8 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 # Event Listeners, might want to seperate into another file when this list gets bigger
 @bot.event
 async def on_ready():
-    bot.add_cog(Commands(bot))
-    bot.add_cog(Prompts(bot))
+    await bot.add_cog(Commands(bot))
+    await bot.add_cog(Prompts(bot))
     print(f'We have logged in as {bot.user}') 
     
 bot.run(token)
